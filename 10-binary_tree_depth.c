532c0f9204925759510670ec66ea4ee1e74c62f0
gichobih <dennismuri254@gmail.com>
2024-06-05 09:13:44 -0700

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+/**
+ * @tree: node to check the depth
+ * Return: 0 is it is the root or number of depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
